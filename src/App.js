79c4c38bdbec723f0e6e029b6c4aff44fc7abf2a
Roefatin Ridzuan <roefatinridzuan@gmail.com>
2022-07-08 01:03:29 +0800

    first revision

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import Typewriter from "typewriter-effect";
+
+import logo from './Photo.jpeg';
+import githublogo from './GitHub-Logo.wine.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className = "App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      <Typewriter
+      onInit={(typewriter)=>{
+        typewriter
+        .typeString("Welcome")
+        .pauseFor(10)
+        .deleteAll()
+        .typeString("to My Portfolio!")
+        .pauseFor(10)
+        .deleteAll()
+        .typeString("I am Roefatin")
+        .pauseFor(10)
+        .deleteAll()
+        .typeString("from Malaysia")
+        .pauseFor(10)
+        .deleteAll()
+        .typeString("Nice to meet you!")
+        .start();
+      }}
+      /> 
+      </p>
+      <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/roewan"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={githublogo} className="githubApp-logo" alt="logo" />
         </a>
       </header>
-    </div>
+      </div>
   );
 }
 
